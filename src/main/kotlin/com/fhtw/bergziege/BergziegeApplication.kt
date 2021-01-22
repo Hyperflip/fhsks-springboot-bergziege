fde38ac03e3cae2f509a255a3e01707da14671b0
Hyperflip <44472080+Hyperflip@users.noreply.github.com>
2021-01-22 16:47:42 +0100

    Initial commit

---
+++ src/main/kotlin/com/fhtw/bergziege/BergziegeApplication.kt
@@ -0,0 +1,11 @@
+package com.fhtw.bergziege
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BergziegeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BergziegeApplication>(*args)
+}
---
+++ src/test/kotlin/com/fhtw/bergziege/BergziegeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fhtw.bergziege
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BergziegeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
